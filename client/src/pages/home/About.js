0a2e53ed1632cb1f733f5d99a7fe943783d6db49
Huzaifa Jalil <huzaiffajalil@gmail.com>
2024-04-25 17:33:28 +0500

    Change about-us text

--- client/src/pages/home/About.js
+++ client/src/pages/home/About.js
@@ -3,48 +3,28 @@ import AboutImg from '../../assets/images/about.jpg';
 
 const About = () => {
     return (
-        <section id="about" className='container mx-auto min-h-screen flex justify-between items-center'>
-            <div className='grid grid-cols-5 space-x-5 md:space-x-10'>
-                <div className='col-span-5 md:col-span-2 mx-5 space-y-2 flex flex-col relative'>
-                    <img src={AboutImg} alt="" className='flex-1 rounded-lg object-cover' />
-                    <div className="absolute w-full left-0 bottom-2">
-                        <div className='mx-2 grid grid-cols-2 gap-2'>
-                            <div className='flex flex-col rounded-lg bg-primary text-secondary px-4 py-6'>
-                                <span className='text-3xl font-semibold'>100+</span>
-                                <span className='text-sm font-light capitalize'>years experience</span>
-                            </div>
-                            <div className='flex flex-col rounded-lg bg-primary text-secondary px-4 py-6'>
-                                <span className='text-3xl font-semibold'>$0</span>
-                                <span className='text-sm font-light capitalize'>premiums</span>
-                            </div>
-                        </div>
-                    </div>
+        <section id="about" className='container mx-auto min-h-screen flex justify-center items-center'>
+            <div className='grid grid-cols-4 space-x-5 md:space-x-10'>
+                <div className='col-span-5 md:col-span-2 mx-5'>
+                    <img src={AboutImg} alt="" className='rounded-lg object-cover' />
                 </div>
-                <div className='col-span-5 md:col-span-3'>
-                    <h3 className='text-base font-bold text-dark capitalize'>About us</h3>
-                    <h2 className='text-4xl font-semibold text-secondary mb-4 uppercase'>Who we are</h2>
+                <div className='col-span-5 md:col-span-2'>
+                <h3 className='text-base font-bold text-dark capitalize'>About us</h3>
+                <h2 className='text-4xl font-semibold text-secondary mb-4 uppercase'>Who we are</h2>
                     <p className='text-dark-color'>
-                        With over a century of combined experience, the Subsidy Searches team
-                        has been helping individuals and families with their insurance needs.
+                        The Subsidy Searches team helps individuals and families with finding low cost insurance coverage.
                     </p>
                     <br />
                     <p className='text-dark-color'>
-                        Our dedicated benefit specialists have assisted hundreds of thousands of people
-                        in finding affordable health insurance plans, often with $0 premiums.
-                        Our goal is to enroll you and your family in a plan that offers the necessary
-                        coverage with maximum benefits at the lowest cost.
+                        Our dedicated benefit specialists have assisted thousands of people in finding affordable health insurance plans,
+                        often with $0 premiums. Our goal is to enroll you and your family in a plan that offers the necessary coverage with
+                        maximum benefits at the lowest cost.
                     </p>
                     <br />
                     <p className='text-dark-color'>
-                        At Subsidy Searches, we firmly believe that EVERYONE should have access to quality health
-                        insurance that fits with their budget. Let us find that plan for you today!
+                        At Subsidy Searches, we firmly believe that EVERYONE should have access to quality health insurance that fits with their
+                        budget. Let us find that plan for you today!
                     </p>
-                    <br />
-                    <p className='text-dark-color'>Sincerely,</p>
-                    <p className='text-dark-color'>
-                        <b>Jean</b>
-                    </p>
-                    <p className='text-dark-color'>Lead Benefit Specialist</p>
                 </div>
             </div>
         </section>
